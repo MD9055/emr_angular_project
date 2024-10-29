c6a559a6e2afb8e14410dc8b2308af2bb1d4d0b2
Pradeep Sahani <pradeep.sudrania@g,ail.com>
2024-10-30 00:35:49 +0530

    data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,10 @@
 export const environment = {
     production: false,
    
-    apiUrl:"https://emr-proj-backend.onrender.com",
-    // apiUrl:"http://localhost:3000",
+    // apiUrl:"https://emr-proj-backend.onrender.com",
+    apiUrl:"http://localhost:3000",
+
+    // apiUrl: "http://159.203.100.155:3000"
 
   
       
